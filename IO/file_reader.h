4f8837e65ad11fe43c133ff6ad4992574efe9d20
rcferreira232 <rafaelcferreira232@gmail.com>
2026-03-10 20:11:37 -0300

    feat: File Reader
    implementação inicial do file_reader

--- IO/file_reader.h
+++ IO/file_reader.h
@@ -1,6 +1,13 @@
 #ifndef FILE_READER_H
 #define FILE_READER_H
 
-int read_file();
+typedef struct {
+    int num_vertices;
+    int num_edges;
+    int **distance_matrix; 
+} GraphData;
+
+GraphData* read_file();
+void free_graph_data();
 
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "IO/file_reader.h"
 
-int main() {
-    printf("GA\n");
+int main() {    
+    GraphData *graph = read_file();
+
+    if (graph) {
+        printf("Número de vértices: %d\n", graph->num_vertices);
+        printf("Número de arestas: %d\n", graph->num_edges);
+        printf("Matriz de distâncias:\n");
+        for (int i = 0; i < graph->num_vertices; i++) {
+            for (int j = 0; j < graph->num_vertices; j++) {
+                printf("%d ", graph->distance_matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    free_graph_data();
+
     return 0;
 }
